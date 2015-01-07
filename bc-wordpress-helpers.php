b401782da4bd58a87e2d585bd6094a67325be669
brandoncamenisch <brandoncamenisch@gmail.com>
2015-01-07 10:23:15 -0700

    Adding base plugin file that is blank

---
+++ bc-wordpress-helpers.php
@@ -0,0 +1,9 @@
+<?php
+/*
+Plugin Name: BC WordPress Helpers
+Plugin URI: http://brandoncamenisch.com
+Description: Some of my custom functions and helpers
+Version: 1.0
+Author: Brandon Camenisch
+Author URI: http://brandoncamenisch.com/
+*/
